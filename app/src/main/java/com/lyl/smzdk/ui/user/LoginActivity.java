9354ca60f48adbdf2ef2f91f848db9a9ffb7ce60
lyl <iamlyl@foxmail.com>
2017-12-04 17:42:42 +0800

    login

---
+++ app/src/main/java/com/lyl/smzdk/ui/user/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.lyl.smzdk.ui.user;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.lyl.smzdk.ui.BaseActivity;
+
+/**
+ * Author: lyl
+ * Date Created : 2017/12/4.
+ */
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
